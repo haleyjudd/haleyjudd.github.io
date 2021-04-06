e3e2dfa76ec805fa8d62b2586b144a024332950c
haley judd <hjudd7@byui.edu>
2021-04-06 15:26:29 -0400

    finalize

--- lesson12/javascript/script.js
+++ lesson12/javascript/script.js
@@ -16,7 +16,7 @@ WebFont.load({
 
 //Home Current Weather
 
-const apiURL = "//api.openweathermap.org/data/2.5/onecall?lat=44.6995&lon=73.4529&appid=2453bb8c49b507f6bde5fe17c37e574e&units=imperial";
+const apiURL = "//api.openweathermap.org/data/2.5/onecall?lat=44.578&lon=-73.4529&appid=2453bb8c49b507f6bde5fe17c37e574e&units=imperial";
 
 //const apiURL = "//api.openweathermap.org/data/2.5/onecall?q=plattsburgh&appid=2453bb8c49b507f6bde5fe17c37e574e&units=imperial";
 
@@ -45,52 +45,9 @@ weekday[3] = "Wednesday";
 weekday[4] = "Thursday";
 weekday[5] = "Friday";
 weekday[6] = "Saturday";
-/*
-const forecastURL = "//api.openweathermap.org/data/2.5/forecast?id=5604473&appid=2453bb8c49b507f6bde5fe17c37e574e&units=imperial";
 
-fetch(forecastURL)
-  .then((response) => response.json())
-  .then((weatherInfo) => {
-    console.log(weatherInfo);
-
-    let mylist = weatherInfo.list;
-    let forecastDayNumber = todayDayNumber;
-
-    for (i = 0; i < todayDayNumber +5; i++) {
-      let time = mylist[i].dt_txt;
-      if (time.includes("21:00:00")) {
-
-        forecastDayNumber += 1;
-        if (forecastDayNumber === 7) {
-          forecastDayNumber = 0;
-        }
-
-        let theDayName = document.createElement("h5");
-        theDayName.textContent = weekday[forecastDayNumber];
-
-        let theTemp = document.createElement("div");
-        theTemp.innerHTML = `${weatherInfo.list[i].main.temp}&#176;F`;
-
-        let iconcode = weatherInfo.list[i].weather[0].icon;
-        let iconPath = "//openweathermap.org/img/w/" + iconcode + ".png";
-        let theIcon = document.createElement("img");
-        theIcon.src = iconPath;
-        theIcon.alt = `Icon image of ${weatherInfo.list[i].weather[0].description}`;
-
-        let theDay = document.createElement("div");
-
-        theDay.append(theDayName);
-        theDay.append(theIcon);
-        theDay.append(theTemp);
-
-        document.getElementById("daysForecast").append(theDay);
-
-      }
-    }
-  });
-*/
   
-  const forecastURL = "//api.openweathermap.org/data/2.5/onecall?lat=44.6995&lon=73.4529&appid=2453bb8c49b507f6bde5fe17c37e574e&units=imperial";
+const forecastURL = "//api.openweathermap.org/data/2.5/onecall?lat=44.578&lon=-73.4529&appid=2453bb8c49b507f6bde5fe17c37e574e&units=imperial";
 
   fetch(forecastURL)
     .then((response) => response.json())
@@ -104,9 +61,7 @@ fetch(forecastURL)
        let time = mylist.daily[i].dt;
         console.log(time);
         console.log(i);
-        if (time.includes("1596564000")) {
           
-        
           forecastDayNumber += 1;
           if (forecastDayNumber === 7) {
             forecastDayNumber = 0;
@@ -116,13 +71,14 @@ fetch(forecastURL)
           theDayName.textContent = weekday[forecastDayNumber];
   
           let theTemp = document.createElement("div");
-          theTemp.innerHTML = `${mylist[i].daily[0].temp.day}&#176;F`;
+          console.log(mylist.daily[i])
+          theTemp.innerHTML = `${mylist.daily[i].temp.day}&#176;F`;
   
-          let iconcode = mylist[i].daily[0].weather[0].icon;
+          let iconcode = mylist.daily[i].weather[0].icon;
           let iconPath = "//openweathermap.org/img/w/" + iconcode + ".png";
           let theIcon = document.createElement("img");
           theIcon.src = iconPath;
-          theIcon.alt = `Icon image of ${mylist[i].daily[0].weather[0].description}`;
+          theIcon.alt = `Icon image of ${mylist.daily[i].weather[0].description}`;
   
           let theDay = document.createElement("div");
   
@@ -133,7 +89,7 @@ fetch(forecastURL)
           document.getElementById("daysForecast").append(theDay);
   
         }
-      }
+      
     });
 
   //Directory-Businesses
