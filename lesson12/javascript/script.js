782e94adeb8d7acc9f2600f1a94cc1455a73f9f9
haley judd <hjudd7@byui.edu>
2021-04-06 11:01:48 -0400

    finalize 3

--- lesson12/javascript/script.js
+++ lesson12/javascript/script.js
@@ -181,4 +181,29 @@ fetch(forecastURL)
    
   });
   
-    
+  /*Dynamic date*/
+const daynames = [
+  "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+const todaysdate = new Date();
+const dayName = daynames[todaysdate.getDay()];
+const monthName = months[todaysdate.getMonth()];
+const year = todaysdate.getFullYear();
+const currentdate = dayName + "-" + " " + monthName + " " + todaysdate.getDate() + "," + " " + year;
+const options = {weekday: 'long', month: 'long', day: 'numeric', year: 'numeric'};
+document.getElementById("currentdate").textContent = currentdate;
+
